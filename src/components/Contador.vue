5d0c6385ab254e7650a984d86f42fd95ed145d1c
JoffreDavid <jdariasb@uce.edu.ec>
2025-11-22 22:46:34 -0500

    Componentes Vue

---
+++ src/components/Contador.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Universidad Central del Ecuador</h1>
+    <h2>Carrera de computación</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {};
+</script>
+
+<style>
+</style>
