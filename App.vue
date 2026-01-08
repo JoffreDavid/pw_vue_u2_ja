a4d52ce91cb243747f1157a77ec8a910db48c5ae
JoffreDavid <167826973+JoffreDavid@users.noreply.github.com>
2026-01-07 19:40:54 -0500

    Prueba App

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- <ContadorView/> -->
+    <!--DirectivasView/--> 
+    <!-- <PreguntaView/> -->
+    <PokemonView/>
+  </div>
+</template>
+
+<script>
+import ContadorView from '../src/views/ContadorView.vue';
+import DirectivasView from './views/DirectivasView.vue';
+import PreguntaView from './views/PreguntaView.vue';
+import PokemonView from "./views/PokemonView.vue"
+export default {
+  components: { 
+    ContadorView, 
+    DirectivasView,
+    PreguntaView,
+    PokemonView
+  },
+};
+</script>
+
+<style >
+  h1 {
+    color: black;
+  } 
+
+</style>
